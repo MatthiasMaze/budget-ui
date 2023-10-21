6d0ce8b94e2d39e14e25132927ea2372204f08f5
MortalMaze <98056118+MortalMaze@users.noreply.github.com>
2023-10-21 15:32:24 +0200

    save added

--- src/app/category/category-modal/category-modal.component.ts
+++ src/app/category/category-modal/category-modal.component.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
-import { ModalController } from '@ionic/angular';
-import { ActionSheetService } from '../../shared/service/action-sheet.service';
-import { filter, from } from 'rxjs';
+import {Component} from '@angular/core';
+import {ModalController} from '@ionic/angular';
+import {ActionSheetService} from '../../shared/service/action-sheet.service';
+import {filter, finalize, from} from 'rxjs';
 import {CategoryService} from "../category.service";
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {ToastService} from "../../shared/service/toast.service";
@@ -13,23 +13,35 @@ import {ToastService} from "../../shared/service/toast.service";
 export class CategoryModalComponent {
   readonly categoryForm: FormGroup;
   submitting = false;
+
   constructor(
     private readonly actionSheetService: ActionSheetService,
     private readonly categoryService: CategoryService,
     private readonly formBuilder: FormBuilder,
     private readonly modalCtrl: ModalController,
     private readonly toastService: ToastService
-
-  ) {  this.categoryForm = this.formBuilder.group({
-    name: ['', [Validators.required, Validators.maxLength(40)]],
-  });}
+  ) {
+    this.categoryForm = this.formBuilder.group({
+      name: ['', [Validators.required, Validators.maxLength(40)]],
+    });
+  }
 
   cancel(): void {
     this.modalCtrl.dismiss(null, 'cancel');
   }
 
   save(): void {
-    this.modalCtrl.dismiss(null, 'save');
+    this.submitting = true;
+    this.categoryService
+      .upsertCategory(this.categoryForm.value)
+      .pipe(finalize(() => (this.submitting = false)))
+      .subscribe({
+        next: () => {
+          this.toastService.displaySuccessToast('Category saved');
+          this.modalCtrl.dismiss(null, 'refresh');
+        },
+        error: (error) => this.toastService.displayErrorToast('Could not save category', error),
+      });
   }
 
   delete(): void {
@@ -39,8 +51,8 @@ export class CategoryModalComponent {
         next: () => {
           this.modalCtrl.dismiss(null, 'delete');
 
-        }
-
+        },
+        error: (error) => this.toastService.displayErrorToast('Could not save category', error),
       });
   }
 }
