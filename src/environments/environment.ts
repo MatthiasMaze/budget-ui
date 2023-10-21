d9529d01318687345d648bff2ceb94b86c37b0a8
MortalMaze <98056118+MortalMaze@users.noreply.github.com>
2023-10-21 14:13:08 +0200

    backendURL added

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,14 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  backendUrl: '/api',
+  production: true,
   firebaseConfig: {
-    apiKey: "AIzaSyCHKc-9GFvrbXMKLlgua2yAtgCibPLddh8",
-    authDomain: "budget-planner-7eddb.firebaseapp.com",
-    projectId: "budget-planner-7eddb",
-    appId: "1:278151755192:web:227ae9460314c348be4b4d",
-  }
+    apiKey: 'AIzaSyCHKc-9GFvrbXMKLlgua2yAtgCibPLddh8',
+    authDomain: 'budget-planner-7eddb.firebaseapp.com',
+    projectId: 'budget-planner-7eddb',
+    appId: '1:278151755192:web:227ae9460314c348be4b4d',
+    backendUrl: '/api',
+  },
 };
 
 /*
