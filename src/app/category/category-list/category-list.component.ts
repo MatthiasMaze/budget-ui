f1eacbac2c92170898e62195c487f962cc416594
MortalMaze <98056118+MortalMaze@users.noreply.github.com>
2023-10-25 20:29:28 +0200

    kategorie liste angepasst

--- src/app/category/category-list/category-list.component.ts
+++ src/app/category/category-list/category-list.component.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
 import { CategoryModalComponent } from '../category-modal/category-modal.component';
 import { ModalController } from '@ionic/angular';
-import { Category } from '../../shared/domain';
+import {Category, CategoryCriteria} from '../../shared/domain';
+import {CategoryService} from "../category.service";
+import {ToastService} from "../../shared/service/toast.service";
 
 @Component({
   selector: 'app-category-list',
   templateUrl: './category-list.component.html',
 })
 export class CategoryListComponent {
-  constructor(private readonly modalCtrl: ModalController) {}
+  categories: Category[] | null = null;
+  readonly initialSort = 'name,asc';
+  lastPageReached = false;
+  loading = false;
+  searchCriteria: CategoryCriteria = { page: 0, size: 25, sort: this.initialSort };
+  constructor(
+    private readonly modalCtrl: ModalController,
+    private readonly categoryService: CategoryService,
+    private readonly toastService: ToastService
+  ) {}
+
 
   async openModal(category?: Category): Promise<void> {
     const modal = await this.modalCtrl.create({ component: CategoryModalComponent });
